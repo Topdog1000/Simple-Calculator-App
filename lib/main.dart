357d5b65964f96380141904b0a9572fab5bb17ca
Topdog1000 <79286487+Topdog1000@users.noreply.github.com>
2022-04-21 20:03:16 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+main()
+{
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp()
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple Calculator App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              "Result : ",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Enter First Number"
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
