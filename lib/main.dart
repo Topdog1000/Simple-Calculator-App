ee0e7a8b939f5e98a4a33d5fb3131c3b03bf78fe
Topdog1000 <79286487+Topdog1000@users.noreply.github.com>
2022-04-21 21:53:07 +0300

    commiting operations buttons and final result

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,45 @@ main()
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  int? result=0, num1=0, num2=0;
+  add(){
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! + num2!;
+    });
+  }
+  sub(){
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! - num2!;
+    });
+  }
+  divi(){
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! ~/ num2!;
+    });
+  }
+  mul(){
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! * num2!;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +58,13 @@ class _MyAppState extends State<MyApp> {
         child: Column(
           children: [
             Text(
-              "Result : ",
+              "Result : $result",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20,
             ),
             TextField(
+              controller: controller1,
               decoration: InputDecoration(
                 labelText: "Enter First Number",
                 border: OutlineInputBorder(
@@ -42,11 +75,44 @@ class _MyAppState extends State<MyApp> {
             SizedBox(height: 20,
             ),
             TextField(
+              controller: controller2,
               decoration: InputDecoration(
                   labelText: "Enter Second Number",
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20))),
             ),
+            SizedBox(height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: (){
+                  add();
+                  controller1.clear();
+                  controller2.clear();
+                }, child: Text("Add")),
+                ElevatedButton(onPressed: (){
+                  sub();
+                  controller1.clear();
+                  controller2.clear();
+                }, child: Text("Subtract")),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: (){
+                  mul();
+                  controller1.clear();
+                  controller2.clear();
+                }, child: Text("Multiply")),
+                ElevatedButton(onPressed: (){
+                  divi();
+                  controller1.clear();
+                  controller2.clear();
+                }, child: Text("Division")),
+              ],
+            )
           ],
         ),
       ),
